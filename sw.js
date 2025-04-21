eca7c7dd9e64ceeec321520b459abd1f31fec26e
Clairefairy <claire.fellix@gmail.com>
2025-04-21 20:52:57 -0300

    Even more fixes

--- sw.js
+++ sw.js
@@ -31,8 +31,3 @@ self.addEventListener("fetch", (event) => {
       .then((response) => (response ? response : fetch(event.request)))
   );
 });
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log('Service Worker registrado com sucesso'))
-    .catch(error => console.log('Erro ao registrar Service Worker:', error));
-};
